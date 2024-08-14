d270defd54a46b6e6df929e93d11a4239764fb8d
Aléxia Vieira Coelho <124118325+alexiiavieirac@users.noreply.github.com>
2024-08-13 21:11:17 -0300

    Adicionando alguns produtos e vendas no dicionário

--- dict.py
+++ dict.py
@@ -4,10 +4,13 @@ sells_tecnology = {"iPhone": 1500, "Samsung Galaxy Book2": 1000, "PS5": 500, "iP
 # Show by private words
 book = sells["Book"]
 cellphone = sells["Tecnology"]
+computer = sells["Computer"]
 
 # Show by sells 
 samsumg_galaxy = sells_tecnology["Samsung Galaxy Book2"]
 video_game = sells_tecnology["PS5"]
+iphone = sells_tecnology["iPhone"]
+ipad = sells_tecnology['iPad']
 
 print("Product:")
 print(f"Cellphone: {cellphone}")
